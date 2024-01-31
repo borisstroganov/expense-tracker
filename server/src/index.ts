12e294a29860b34488aae1e663001f0c1f8615c0
Boris Stroganov <k20040031@kcl.ac.uk>
2024-01-31 20:13:58 +0000

    Begin initial set up

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+export const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(5000, () => {
+    console.log('App has started');
+});
